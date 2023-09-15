2bd09a1e7dde8fff8747ad78bfdabdb917968509
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-09-15 14:30:33 +0800

    chore: finish

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 use num_traits::Num;
-use std::ops::{Add, AddAssign, Index, IndexMut, Mul, Sub, SubAssign};
+use std::{
+    fmt::{Debug, Display},
+    ops::{Add, AddAssign, Index, IndexMut, Mul, Sub, SubAssign},
+};
 
 pub enum Assert<const CHECK: bool> {}
 
@@ -12,20 +15,37 @@ impl IsTrue for Assert<true> {}
 #[derive(Debug, Clone, Copy)]
 struct Matrix<T, const X: usize, const Y: usize>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     inner: [[T; X]; Y],
 }
 
 impl<T, const X: usize> Matrix<T, X, X>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
-    fn cut_index(&self, [x, y]: [usize; 2]) -> Matrix<T, { X - 1 }, { X - 1 }>
+    fn cut_index(&self, [input_x, input_y]: [usize; 2]) -> Matrix<T, { X - 1 }, { X - 1 }>
     where
         [(); X - 1]:,
     {
-        todo!()
+        let mut index_y = 0;
+        let matrix_inner = self.matrix();
+        let mut output = Matrix::empty();
+        for (y, line) in matrix_inner.iter().enumerate() {
+            if y == input_y {
+                continue;
+            }
+            index_y += 1;
+            let mut index_x = 0;
+            for (x, element) in line.iter().enumerate() {
+                if x == input_x {
+                    continue;
+                }
+                index_x += 1;
+                output[[index_y, index_x]] = *element;
+            }
+        }
+        output
     }
 }
 
@@ -38,19 +58,18 @@ where
     Assert<{ X > 2 }>: IsTrue,
     Matrix<T, { X - 1 }, { X - 1 }>: Determinant<T>,
     [(); X - 1]:,
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn determinant(&self) -> T {
         let mut output = num_traits::zero();
-        let matrix = self.matrix();
-
         for x in 0..X {
-            let current: Matrix<T, { X - 1 }, { X - 1 }> = Matrix::empty();
+            let current = self.cut_index([x, 0]);
             if x % 2 == 0 {
-                output += (current * self[[0, x]]).determinant();
+                output += current.determinant() * self[[1, x + 1]];
             } else {
-                output -= (current * self[[0, x]]).determinant();
+                output -= current.determinant() * self[[1, x + 1]];
             }
+            println!("output is  {output}");
         }
         output
     }
@@ -58,7 +77,7 @@ where
 
 impl<T> Determinant<T> for Matrix<T, 0, 0>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn determinant(&self) -> T {
         num_traits::zero()
@@ -67,25 +86,25 @@ where
 
 impl<T> Determinant<T> for Matrix<T, 1, 1>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn determinant(&self) -> T {
-        self[[0, 0]]
+        self[[1, 1]]
     }
 }
 
 impl<T> Determinant<T> for Matrix<T, 2, 2>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn determinant(&self) -> T {
-        self[[0, 0]] * self[[1, 1]] - self[[0, 1]] * self[[1, 0]]
+        self[[1, 1]] * self[[2, 2]] - self[[1, 2]] * self[[2, 1]]
     }
 }
 
 impl<T, const X: usize, const Y: usize> Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn empty() -> Self {
         Matrix {
@@ -109,7 +128,7 @@ where
 
 impl<T, const X: usize, const Y: usize> Mul<T> for Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     type Output = Matrix<T, X, Y>;
     fn mul(self, rhs: T) -> Self::Output {
@@ -126,7 +145,7 @@ where
 
 impl<T, const X: usize, const Y: usize> Add<Matrix<T, X, Y>> for Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     type Output = Matrix<T, X, Y>;
     fn add(self, rhs: Matrix<T, X, Y>) -> Self::Output {
@@ -143,7 +162,7 @@ where
 
 impl<T, const X: usize, const Y: usize> Sub<Matrix<T, X, Y>> for Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     type Output = Matrix<T, X, Y>;
     fn sub(self, rhs: Matrix<T, X, Y>) -> Self::Output {
@@ -161,7 +180,7 @@ where
 impl<T, const X1: usize, const Y1: usize, const X2: usize> Mul<Matrix<T, X2, X1>>
     for Matrix<T, X1, Y1>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     type Output = Matrix<T, X2, Y1>;
     fn mul(self, rhs: Matrix<T, X2, X1>) -> Self::Output {
@@ -182,7 +201,7 @@ where
 
 impl<T, const X: usize, const Y: usize> Index<[usize; 2]> for Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     type Output = T;
     fn index(&self, [x, y]: [usize; 2]) -> &Self::Output {
@@ -195,7 +214,7 @@ where
 
 impl<T, const X: usize, const Y: usize> IndexMut<[usize; 2]> for Matrix<T, X, Y>
 where
-    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub,
+    T: Num + Default + Copy + AddAssign + SubAssign + Add + Sub + Display + Debug,
 {
     fn index_mut(&mut self, [x, y]: [usize; 2]) -> &mut Self::Output {
         assert!(x != 0 && y != 0);
@@ -219,4 +238,9 @@ fn main() {
     println!("{:?}", b);
 
     println!("{:?}", a * b);
+
+    let e: Matrix<i32, 3, 3> = Matrix::new([[1, 2, 1], [0, 3, 4], [3, 1, 4]]);
+    println!("{}", e.determinant());
+    let f: Matrix<i32, 3, 3> = Matrix::new([[1, 0, 3], [-1, -1, -3], [0, 0, 6]]);
+    println!("{}", f.determinant());
 }
